062a53864bf4e0bb08f997477e665ab003388c67
Armaan Gupta <armaan@guppy.im>
2023-02-23 13:31:24 -0600

    feat: more more setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import logger from '../utils/logger';
 
 const PORT = process.env.PORT || 3000;
 
