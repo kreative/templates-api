08f38b9681dca2ae7eb05ca010f5d05741791590
Armaan Gupta <armaan@guppy.im>
2023-02-23 12:42:31 -0600

    feat: added stufffffffffff

---
+++ utils/logger.ts
@@ -0,0 +1,18 @@
+import { createLogger, transports, format } from 'winston';
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf(({ timestamp, level, message, service }) => {
+      return `[${timestamp}] ${service} ${level}: ${message}`;
+    }),
+  ),
+  transports: [new transports.Console()],
+  defaultMeta: {
+    service: 'KreativeIdApi',
+  },
+});
+
+export default logger;
