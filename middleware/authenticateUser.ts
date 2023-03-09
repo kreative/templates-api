ab5dfb3644785597144f72835a39e3ee2a652eb7
Armaan Gupta <armaan@guppy.im>
2023-03-09 01:34:01 -0600

    ref: swapped back to axios

--- middleware/authenticateUser.ts
+++ middleware/authenticateUser.ts
@@ -1,6 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import wretch from 'wretch';
+import axios from 'axios';
 import logger from '../utils/logger';
 
 const requiredPermissions: string[] = ['KREATIVE_TEMPLATES_USER'];
@@ -29,85 +29,119 @@ export class AuthenticateUserMiddleware implements NestMiddleware {
       });
     }
 
-    wretch('https://id-api.kreativeusa.com/v1/keychains/verify')
-      .post({
+    // verify the key using an AXIOS request (not using the Keychain Service)
+    axios
+      .post(`https://id-api.kreativeusa.com/v1/keychains/verify`, {
         key,
         aidn,
         appchain,
       })
-      .internalError((error) => {
-        logger.warn('authenticate user middleware sent 500 error', error);
-        res.status(500).send({
-          statusCode: 500,
-          message: 'internal server error',
-        });
-      })
-      .forbidden((error) => {
-        logger.warn('authenticate user middleware sent with 403 error', error);
-        res.status(403).send({
-          statusCode: 403,
-          message: 'aidn mismatch',
-        });
-      })
-      .unauthorized((error) => {
-        logger.warn('authenticate user middleware sent 400 error', error);
-        res.status(400).send({
-          statusCode: 400,
-          message: 'unauthorized',
-        });
-      })
-      .notFound((error) => {
-        logger.warn('authenticate user middleware sent 404 error', error);
-        res.status(404).send({
-          statusCode: 404,
-          message: 'missing account or keychain',
-        }); 
-      })
-      .json((response) => {
-        // verifies that the user has the neccessary permissions
-        const permissions: string[] = response.data.account.permissions;
+      .then((response) => {
+        // status code is between 200-299
+        if (response.data.statusCode === 200) {
+          // verifies that the user has the neccessary permissions
+          const permissions: string[] = response.data.data.account.permissions;
 
-        // checks to see if the user's permissions include the ones required
-        // in other Kreative applications this will have to be manually configured based on number of permissions
-        // we can't just say the user isn't authenticated, because they are, they just don't have the correct permissions
-        // therefore we will throw a ForbiddenException with a custom message about permissions
-        if (
-          !(
-            permissions.includes(requiredPermissions[0]) ||
-            permissions.includes(requiredPermissions[1])
-          )
-        ) {
-          // user does not have the correct permissions to continue with the request
-          logger.warn('authenticate middleware sent 403 error', {
-            userPermissions: permissions,
-            requiredPermissions,
-          });
-          res.status(403).send({
-            statusCode: 403,
-            message: 'user doesnt have correct permissions',
-          });
-        } else {
-          // checks to see if the AIDN on the keychain is the same AIDN as our application (Kreative ID non-test)
-          // this is checking to see if the user on the client is sending through a keychain that was
-          // created when they tried signing into this application
-          // parses the environment variable for the HOST_AIDN
-          const HOST_AIDN: number = parseInt(process.env.HOST_AIDN);
-
-          if ((response.data.keychain.aidn as number) !== HOST_AIDN) {
-            // sends back an UnauthorizedException
-            logger.warn('authenticate middleware sent 401 error', {
-              hostAidn: HOST_AIDN,
-              givenAidn: response.data.data.keychain.aidn,
+          // checks to see if the user's permissions include the ones required
+          // in other Kreative applications this will have to be manually configured based on number of permissions
+          // we can't just say the user isn't authenticated, because they are, they just don't have the correct permissions
+          // therefore we will throw a ForbiddenException with a custom message about permissions
+          if (
+            !(
+              permissions.includes(requiredPermissions[0]) ||
+              permissions.includes(requiredPermissions[1])
+            )
+          ) {
+            // user does not have the correct permissions to continue with the request
+            logger.warn({
+              message: 'authenticate middleware send 401 error',
+              userPermissions: permissions,
+              requiredPermissions,
             });
-            res.status(401).send({
-              statusCode: 401,
-              message: 'keychain.aidn does not match HOST_AIDN',
+            res.status(403).send({
+              statusCode: 403,
+              message: 'user doesnt have correct permissions',
             });
           } else {
-            // calls next() once everything passes
-            logger.info('authenticate middleware passed');
-            next();
+            // checks to see if the AIDN on the keychain is the same AIDN as our application (Kreative ID non-test)
+            // this is checking to see if the user on the client is sending through a keychain that was
+            // created when they tried signing into this application
+            // parses the environment variable for the HOST_AIDN
+            const HOST_AIDN: number = parseInt(process.env.HOST_AIDN);
+
+            if ((response.data.data.keychain.aidn as number) !== HOST_AIDN) {
+              // sends back an UnauthorizedException
+              logger.warn({
+                message: 'authenticate middleware sent 401 error',
+                hostAidn: HOST_AIDN,
+                givenAidn: response.data.data.keychain.aidn,
+              });
+              res.status(401).send({
+                statusCode: 401,
+                message: 'keychain.aidn does not match HOST_AIDN',
+              });
+            } else {
+              // calls next() once everything passes
+              logger.info('authenticate middleware passed');
+              next();
+            }
           }
+        }
+      })
+      .catch((error) => {
+        // status code is not between 200-299
+        console.log('error: corey is awesome');
+        const statusCode = error.response.status;
+
+        if (statusCode === 404) {
+          // NotFoundException, either account or key isn't found
+          // either way something is majorly incorrect so we have to throw an error
+          logger.warn({
+            message:
+              'authenticate user middleware failed with 404 error for missing account or application',
+            error,
+          });
+          res.status(404).send({
+            statusCode: 404,
+            message: error.response.data.message,
+          });
+        } else if (statusCode === 401) {
+          // UnauthorizedException (the keychain is expired)
+          // since the user is trying to make a request with an expired keychain we throw another UnauthorizedException
+          logger.warn({
+            message:
+              'authenticate user middleware failed with 401 error for expired keychain',
+            error,
+          });
+          res
+            .status(401)
+            .send({ statusCode: 401, message: 'expired keychain' });
+        } else if (statusCode === 403) {
+          // ForbiddenException (aidn mismatch)
+          logger.warn({
+            message:
+              'authenticate user middleware failed with 403 error for aidn mismatch',
+            error,
+          });
+          res.status(403).send({ statusCode: 403, message: 'aidn mismatch' });
+        } else if (statusCode === 500) {
+          // InternalServerException
+          logger.warn({
+            message:
+              'authenticate user middleware failed with 500 error for internal server error',
+            error,
+          });
+          res
+            .status(500)
+            .send({ statusCode: 500, message: 'error from server side' });
+        } else {
+          // some unknown error through unknown status code
+          logger.error({
+            message:
+              'authenticate user middleware failed with 500 error for unknown error',
+            error,
+          });
+          res.status(500).send({ statusCode: 500, message: 'unknown error' });
         }
       });
   }
