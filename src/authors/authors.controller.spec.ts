7f035f0703ba4e7c30f394a8a79fea7d4739b74f
Armaan Gupta <armaan@guppy.im>
2023-03-08 22:27:55 -0600

    fix: base tests for authors api

--- src/authors/authors.controller.spec.ts
+++ src/authors/authors.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorsController } from './authors.controller';
+import { AuthorsService } from './authors.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('AuthorsController', () => {
   let controller: AuthorsController;
@@ -7,6 +9,7 @@ describe('AuthorsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthorsController],
+      providers: [AuthorsService, PrismaService],
     }).compile();
 
     controller = module.get<AuthorsController>(AuthorsController);
--- src/authors/authors.service.spec.ts
+++ src/authors/authors.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorsService } from './authors.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('AuthorsService', () => {
   let service: AuthorsService;
@@ -7,6 +8,7 @@ describe('AuthorsService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthorsService],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<AuthorsService>(AuthorsService);
