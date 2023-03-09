39fd199a4aab185cd7daa5a947f7d0c57e2dade5
Armaan Gupta <armaan@guppy.im>
2023-03-08 22:28:15 -0600

    fix: base test for downloads api

--- src/downloads/downloads.controller.spec.ts
+++ src/downloads/downloads.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DownloadsController } from './downloads.controller';
+import { DownloadsService } from './downloads.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('DownloadsController', () => {
   let controller: DownloadsController;
@@ -7,6 +9,7 @@ describe('DownloadsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DownloadsController],
+      providers: [DownloadsService, PrismaService],
     }).compile();
 
     controller = module.get<DownloadsController>(DownloadsController);
--- src/downloads/downloads.service.spec.ts
+++ src/downloads/downloads.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DownloadsService } from './downloads.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('DownloadsService', () => {
   let service: DownloadsService;
@@ -7,6 +8,7 @@ describe('DownloadsService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [DownloadsService],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<DownloadsService>(DownloadsService);
